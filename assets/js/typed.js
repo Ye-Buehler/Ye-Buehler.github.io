b7aacae5f5e3559242cb5c6393bd66ebd542bc05
Ye Buehler <93389795+Ye-Buehler@users.noreply.github.com>
2024-11-09 21:32:45 +0100

    Delete assets/js/typed.js

--- assets/js/typed.js
+++
@@ -1,8 +0,0 @@
-var options = {
-  strings: ["an Adventurer", "a Learner", "an Explorer"], // Texts to type
-  typeSpeed: 100,  // Speed of typing
-  backSpeed: 50,   // Speed of deleting
-  loop: true       // Loop typing animation
-};
-
-var typed = new Typed(".type", options);
