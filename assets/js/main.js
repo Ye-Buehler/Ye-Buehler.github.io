14e6b2106267ece01afffacae8ace9916af1e117
Ye Buehler <93389795+Ye-Buehler@users.noreply.github.com>
2024-11-09 20:54:37 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,16 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+
+		// Typing Animation using Typed JS
+		var typed = new Typed(".type", {
+		  strings: ["a Web", "an Android", "a Blockchain"],
+		  smartBackspace: true,
+		  startDelay: 1000,
+		  typeSpeed: 130,
+		  backDelay: 1000,
+		  backSpeed: 60,
+		  loop: true,
+		});
+
 })(jQuery);
