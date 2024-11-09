17dfda6a27c81682601cbed7ca4cccd211c03b07
Ye Buehler <93389795+Ye-Buehler@users.noreply.github.com>
2024-11-09 21:33:54 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,15 +399,4 @@
 					});
 
 
-		// Typing Animation using Typed JS
-		var typed = new Typed(".type", {
-		  strings: ["a Novel Drug", "a Chemical Space", "a ML/NLP"],
-		  smartBackspace: true,
-		  startDelay: 1000,
-		  typeSpeed: 130,
-		  backDelay: 1000,
-		  backSpeed: 60,
-		  loop: true,
-		});
-
 })(jQuery);
